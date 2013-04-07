b664e48475391c0cbd2b600169a9266ba6d03b8e
chralden <chralden@gmail.com>
2013-04-07 17:38:42 +0200

    turned each staff object into individual canvas element that will function independently

--- js/notation.js
+++ js/notation.js
@@ -2,23 +2,9 @@
 var FUX = (function (fux) {
 	
 	var notation = function(){
-		
-		//The canvas element
-		var theCanvas,
-
-		//the canvas "context"
-		context,
-
-		//Track the users mouse
-		mouse = { x: 0, y: 0 },
-
-		//The dimensions of the canvas element
-		cWidth,
-		cHeight,
 
 		//objects for rendering staves and notes
-		staff,
-		note,
+		var staff,
 
 		//The current note type for the tooltip
 		currentNoteValue = 'whole',
@@ -51,56 +37,35 @@ var FUX = (function (fux) {
 			}
 		},
 
-		getPitchFromPosition = function(clef){
-			var thisPitch = false,
-			staffOffset = 23;
-
-			$.each(pitchMappings[clef], function(pitch, position){
-				if(mouse.y - staffOffset >= position - 5 && mouse.y - staffOffset <= position + 5){
-					thisPitch = pitch;
-				}
-			});
-
-			return thisPitch;
-		},
-
-		//Event listeners to track mouse position and mouse clicks within the notation canvas
-		onMouseMove = function(e) {
-			mouse.x = e.clientX - theCanvas.offsetLeft;
-			mouse.y = e.clientY - theCanvas.offsetTop;
-
-		},
-
-		//Setup the Canvas
-		setup = function(){
-			theCanvas = document.getElementById("fux-notation");
-			context = theCanvas.getContext("2d");
-
-			cWidth = theCanvas.width;
-			cHeight = theCanvas.height;
-		};
-
 		//an object to render a note to the canvas
 		note = {
 			noteImages: {
 				whole: 'images/whole-note.png'
 			},
 			pitch: 'a4',
+			duration: 'whole',
 
 			//Dimensions of the note image, defaults to whole note image dimensions
 			width: 33,
 			height: 48,
 
 			//Initialize and render the note
-			create: function(pitch, duration, position, clef){
-				this.pitch = pitch;
-				this.duration = duration;
+			create: function(options){
 
-				this.render(position, clef);
+				var self = this,
+				options = options || false,
+				context = (options && options.context) ? options.context : false,
+				position = (options && options.position) ? options.position : false,
+				clef = (options && options.clef) ? options.clef : false;
+
+				if(options && options.pitch !== undefined) self.pitch = options.pitch;
+				if(options && options.duration !== undefined) self.duration = options.duration;
+
+				if(context && position && clef) self.render(context, position, clef);
 			},
 
 			//Render the note in it's position in the appropriate measure
-			render: function(position, clef){
+			render: function(context, position, clef){
 				var self = this,
 				noteImage = new Image();
 
@@ -131,11 +96,25 @@ var FUX = (function (fux) {
 
 		//an object to render a staff to the canvas
 		staff = {
-			//Default X position, Y position, width, and height for a musical staff
+			
+			//The canvas element
+			theCanvas: '',
+
+			//The canvas "context"
+			context: '',
+
+			//The target element for the canvas and the staffs name
+			target: false,
+			name: false,
+
+			//Track the users mouse
+			mouse: { x: 0, y: 0 },
+
+			//Default X position, Y position, width, and height for a musical staff canvas
 			x: 0,
-			y: 0,
+			y: 40,
 			width: 500,
-			height: 90,
+			height: 170,
 
 			//Clef type of the staff, defaults as treble
 			clef: 'treble',
@@ -145,18 +124,34 @@ var FUX = (function (fux) {
 			measures: [],
 
 			//The current measure and beat position on the staff
-			currentPosition: {
-				measure: 0,
-				beat: 0
-			},
+			currentMeasure: 0,
+			currentBeat: 0,
 
 			//Default images for staff background an measure bars
 			image: 'images/stave1.jpg',
 			measureBar: 'images/measure-delim.png',
 
+			getPitchFromPosition: function(clef){
+				var self = this,
+				thisPitch = false,
+				staffOffset = 23;
+
+				$.each(pitchMappings[clef], function(pitch, position){
+				
+					if(self.mouse.y >=  staffOffset + position - 5 && self.mouse.y <= staffOffset + position + 5){
+						thisPitch = pitch;
+					}
+				});
+
+
+				return thisPitch;
+
+			},
+
 			onMouseClick: function(self){
-				thisPitch = getPitchFromPosition(self.clef)
-				if(self.currentPosition.measure < self.measures.length && thisPitch){
+				thisPitch = self.getPitchFromPosition(self.clef);
+				
+				if(self.currentMeasure < self.measures.length && thisPitch){
 					self.addNote({
 						pitch: thisPitch,
 						duration: currentNoteValue
@@ -164,40 +159,98 @@ var FUX = (function (fux) {
 				}
 			},
 
-			//Initialize a staff with given, x, y, width, and measure numbers, then render
-			create: function(x, y, width, measureLength){
-				var self = this,
-				i, startOfMeasure, measureOffset;
+			//Event listeners to track mouse position and mouse clicks within the notation canvas
+			onMouseMove: function(self, e){
+				var rect = self.theCanvas.getBoundingClientRect();
 
-				self.x = x;
-				self.y = y;
-				self.width = width;
-				self.measureLength = measureLength;
+		        self.mouse.x = e.clientX - rect.left;
+		        self.mouse.y = e.clientY - rect.top;
+			},
 
-				measureOffset = self.x;
+			//Setup the Canvas
+			setup: function(){
+				var self = this;
 
-				theCanvas.addEventListener("click", 
+				//add the staff canvas element to the target element
+				self.target.append('<canvas id="'+self.name+'"></canvas>');
+
+				//Initialize the 
+				self.theCanvas = document.getElementById(self.name);
+				self.context = self.theCanvas.getContext("2d");
+
+				self.theCanvas.width = self.width;
+				self.theCanvas.height = self.height;
+
+			},
+
+			//Add Event Listeners to the staff Canvas
+			bindEvents: function(){
+				var self = this;
+
+				self.theCanvas.addEventListener("mouseover", function(){
+					$(self.theCanvas).bind("mousemove", function(e){
+						self.onMouseMove(self, e);
+					});
+				});
+
+				self.theCanvas.addEventListener("mouseout", function(){
+					$(self.theCanvas).unbind("mousemove", function(e){
+						self.onMouseMove(self, e);
+					});
+				});
+
+				//theCanvas.addEventListener("mousemove", onMouseMove, false);
+				self.theCanvas.addEventListener("click", 
 											function(){
 												self.onMouseClick(self);
 											}, 
 											false);
+			},
 
-				//Initialize all the measure objects
-				if(self.measureLength > 0){
-					for(i = 0; i < (self.measureLength); i++){
-						startOfMeasure = measureOffset;
-						measureOffset += self.width/self.measureLength;
-						self.measures[i] = {
-							start: startOfMeasure,
-							end: measureOffset,
-							width: self.width/self.measureLength,
-							//Time signature value = 4 quarter notes, as all exercises are in common time
-							value: 4
-						};
+			//Initialize a staff with given, x, y, width, and measure numbers, then render
+			create: function(options){
+
+				var self = this,
+				options = options || false,
+				measures = [],
+				i, startOfMeasure, measureOffset;
+
+				//If passed as options reset default object properties
+				if(options && options.x) self.x = options.x;
+				if(options && options.y) self.y = options.y;
+				if(options && options.width) self.width = options.width;
+				if(options && options.height) self.width = options.height;
+				if(options && options.measureLength) self.measureLength = options.measureLength;
+				if(options && options.target) self.target = options.target;
+				if(options && options.name) self.name = options.name;
+
+				measureOffset = self.x;
+				
+				//If a target element has been set, setup canvas and create measures and render
+				if(self.target && self.name){
+
+					self.setup();
+
+					self.bindEvents();
+
+					//Initialize all the measure objects
+					if(self.measureLength > 0){
+						for(i = 0; i < (self.measureLength); i++){
+							startOfMeasure = measureOffset;
+							measureOffset += self.width/self.measureLength;
+							measures[i] = {
+								start: startOfMeasure,
+								end: measureOffset,
+								width: self.width/self.measureLength,
+								//Time signature value = 4 quarter notes, as all exercises are in common time
+								value: 4
+							};
+						}
 					}
+					self.measures = measures;
+					self.render();
 				}
-
-				self.render();
+				
 			},
 
 			//Render the staff images to the canvas
@@ -212,20 +265,21 @@ var FUX = (function (fux) {
 
 				//Render staff background image
 				staffImage.onload = function() {
-					context.drawImage(staffImage, self.x, self.y, self.width, self.height);
+					self.context.drawImage(staffImage, self.x, self.y, self.width, 90);
 
 					//Render bar lines
 					measureBarImage.onload = function() {
 						var i;
 
 						//Render opening bar
-						context.drawImage(measureBarImage, self.x, self.y);
+						self.context.drawImage(measureBarImage, self.x, self.y);
 
 						//If the staff contains measures, display the correct number of bar lines and create measure data objects
-						for(i = 0; i < self.measures.length; i++){
-							context.drawImage(measureBarImage, self.measures[i].end, self.y + 1);
+						for(i = 0; i < self.measures.length - 1; i++){
+							self.context.drawImage(measureBarImage, self.measures[i].end, self.y + 1);
 						}
 
+						self.context.drawImage(measureBarImage, self.x + self.width - 4, self.y + 2);
 					};
 				};
 	
@@ -234,13 +288,14 @@ var FUX = (function (fux) {
 			addNote: function(n){
 				var self = this,
 				thisNote = object(note),
-				thisMeasure = this.measures[self.currentPosition.measure];
+				thisMeasure = self.measures[self.currentMeasure];
 
 				//Place the note to center given the position in the current measure
 				notePosition = (thisMeasure.start + (thisMeasure.width/2)) - (thisNote.width/2);
 
-				thisNote.create(n.pitch, n.duration, notePosition, self.clef, self.y);
-				self.currentPosition.measure++;
+				thisNote.create({ context: self.context, pitch: n.pitch, duration: n.duration, position: notePosition, clef: self.clef });
+				
+				self.currentMeasure++;
 			}
 		};
 		
@@ -248,15 +303,14 @@ var FUX = (function (fux) {
 		return {
 
 			init: function(){
-				var s1;
-
-				setup();
+				var s1, s2, s3, s4;
 
 				s1 = object(staff);
-				s1.create(20, 40, 960, 4);
+				s2 = object(staff);
+				s3 = object(staff);
 
-				theCanvas.addEventListener("mousemove", onMouseMove, false);
-				
+				s1.create({ name: 'treble1', target: $('#fux-notation'), width: 960, measureLength: 5 });
+				s2.create({ name: 'treble2', target: $('#fux-notation'), width: 960, measureLength: 5 });
 			}
 		}
 		
